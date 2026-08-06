8d68755a01af1c34c272a269546c9432b6166c00
steineggan <steineggan1@gmail.com>
2026-08-06 10:07:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
     ],
 
     2: [
-      'images/Brazil_jan2015.jpg',
+      'images/travel-notes/Brazil_jan2015.jpg',
       'images/travel2.jpg',
       'images/travel3.jpg'
     ],
